f1393edf302a8d05dfb38e4f160ccfc581d39cf6
kelvyn <kudufo@gmail.com>
2023-06-15 20:31:38 +0100

    made use of dom in redering RPS game

--- app.js
+++ app.js
@@ -1,46 +1,54 @@
-const comChoice = ["ROCK", "PAPPER", "SCISSORS"];
-const winners = [];
+const playGame = document.querySelector(".playRound");
+const games = document.querySelectorAll(".game");
+const com = document.querySelector(".com");
+const play = document.querySelector(".play");
+const draw = document.querySelector(".draw");
+const roundWin = document.querySelector(".roundWin");
+const comFinal = document.querySelector(".comFinal");
+const playFinal = document.querySelector(".playFinal");
+const tieFinal = document.querySelector(".tieFinal");
+const resets = document.querySelector(".resets");
+const finalRes = document.querySelector(".finalRes");
+const btns = document.querySelectorAll(".btns");
+
+const comChoice = ["ROCK", "PAPER", "SCISSORS"];
+let winners = [];
+let playCount = 0;
 
 function getComputerChoice() {
   return comChoice[Math.floor(Math.random() * comChoice.length)];
 }
-function declareWinner(choiceP, choiceC) {
+function playRound(choiceP, choiceC) {
   if (choiceP === choiceC) {
     return "TIE";
   } else if (
-    (choiceP === "SCISSORS" && choiceC === "PAPPER") ||
+    (choiceP === "SCISSORS" && choiceC === "PAPER") ||
     (choiceP === "ROCK" && choiceC === "SCISSORS") ||
-    (choiceP === "PAPPER" && choiceC === "ROCK")
+    (choiceP === "PAPER" && choiceC === "ROCK")
   ) {
     return "PLAYER";
   } else {
     return "COMPUTER";
   }
 }
-function playerSelection() {
-  let input = prompt("Kindly choose between ROCK,PAPER and SCISSORS");
-  while (input == null) {
-    input = prompt("Kindly choose between ROCK,PAPER and SCISSORS");
-  }
-  input = input.toUpperCase();
-  return input;
-}
 
-function playRound() {
-  const playerPlay = playerSelection();
+function gameStart(playerSelection) {
+  const playerPlay = playerSelection.toUpperCase();
   const computerSelection = getComputerChoice();
-  const winner = declareWinner(playerPlay, computerSelection);
+  const winner = playRound(playerPlay, computerSelection);
   winners.push(winner);
   logWinner(playerPlay, computerSelection, winner);
 }
 
-function logWinner(playerSelection, computerSelection, winner) {
-  console.log("PLAYER CHOSE: ", playerSelection);
+function logWinner(playerPlay, computerSelection, winner) {
+  play.textContent = `PLAYER CHOSE: ${playerPlay}`;
+  com.textContent = `COMPUTER CHOSE: ${computerSelection}`;
+  console.log("PLAYER CHOSE: ", playerPlay);
   console.log("COMPUTER CHOSE: ", computerSelection);
   if (winner === "TIE") {
-    console.log("ITS A TIE!");
+    roundWin.textContent = "ITS A TIE";
   } else {
-    console.log(winner, "WINS THIS ROUND");
+    roundWin.textContent = `${winner} WINS THIS ROUND`;
   }
   console.log("...............................");
 }
@@ -48,15 +56,40 @@ function finalResult() {
   let playerResult = winners.filter((item) => item === "PLAYER").length;
   let computerResult = winners.filter((item) => item === "COMPUTER").length;
   let tieResult = winners.filter((item) => item === "TIE").length;
-  console.log("PLAYER WIN: ", playerResult);
-  console.log("COMPUTER WINS: ", computerResult);
-  console.log("TIE: ", tieResult);
+  console.log(winners);
+  playFinal.textContent = `PLAYER WIN: ${playerResult}`;
+  comFinal.textContent = `COMPUTER WIN: ${computerResult}`;
+  tieFinal.textContent = `TIE: ${tieResult}`;
+  if (playerResult > computerResult) {
+    finalRes.textContent = "PLAYER WIN! HURRAY CONGRATULATIONS";
+    finalRes.style.color = "green";
+  } else if (playerResult < computerResult) {
+    finalRes.textContent = "COMPUTER WIN! TRY AGAIN LATER";
+    finalRes.style.color = "red";
+  } else {
+    finalRes.textContent = "ITS A TIE! NO WINNER";
+  }
+}
+function resetBtn(playerSelection){
+  btns.forEach(btn => btn.disabled = false)
+  game(playerSelection)
 }
 
-function game() {
-  for (let i = 1; i <= 5; i++) {
-    playRound(i);
-  }
-  finalResult();
+function disableBtn() {
+  btns.forEach(btn => btn.disabled = true)
+  
 }
-game();
+function game(e) {
+  playCount++;
+  gameStart(e.target.value);
+  if (playCount == 5) {
+    finalResult();
+    playCount = 0;
+    disableBtn();
+  }
+}
+
+games.forEach((button) => {
+  button.addEventListener("click", game);
+});
+resets.addEventListener("click", resetBtn);
